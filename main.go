8b081450e99cf32945e9ed59fec33bb31725c6a0
Lúcio do Nascimento <luciomate95@gmail.com>
2022-05-31 23:47:17 -0300

    Criaçao do modelos de personalidades

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"api-go-rest/models"
 	"api-go-rest/routes"
 	"fmt"
 )
 
 func main() {
+  models.Personalidades =[]models.Personalidade {
+		{Nome: "Nome 1", Historia: "Historia 1"}, 
+		{Nome: "Nome 2", Historia: "Historia 2"}, 
+
+	}
+
+
 	fmt.Println("Iniciando o servidor Rest com Go")
 	routes.HandleRequest()
 }
+  

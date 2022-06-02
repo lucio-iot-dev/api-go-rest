b74a0b8fe1a7ac59dd937a4c8ed9c46e45abb249
Lúcio do Nascimento <luciomate95@gmail.com>
2022-06-01 23:48:53 -0300

    iserçao do banco de dados e docker

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -5,12 +5,26 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Home Page")
 }
 
-func TodasPersonalidades(w http.ResponseWriter, r *http.Request)  {
-	 json.NewEncoder(w).Encode(models.Personalidades)
+func TodasPersonalidades(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(models.Personalidades)
+}
+
+func RetornaUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	for _, personalidade := range models.Personalidades {
+		if strconv.Itoa(personalidade.Id) == id {
+			json.NewEncoder(w).Encode(personalidade)
+		}
+	}
 }
--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 )
 
 func main() {
-  models.Personalidades =[]models.Personalidade {
-		{Nome: "Nome 1", Historia: "Historia 1"}, 
-		{Nome: "Nome 2", Historia: "Historia 2"}, 
+  models.Personalidades = []models.Personalidade {
+		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"}, 
+		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"}, 
 
 	}
 
